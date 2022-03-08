ed0df38d094cf986587321e286d501f9508c90f3
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-07 22:31:03 -0300

    Exercício 2 adicionado

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -1,8 +1,20 @@
-let a = 2;
+// Exercício 1
+/*let a = 2;
 let b = 3;
 
 console.log(a+b);
 console.log(a-b);
 console.log(a*b);
 console.log(a/b);
-console.log(a%b);
+console.log(a%b);*/
+
+// Exercício 2
+let a = 5;
+let b = 2;
+
+if(a>b) {
+  console.log(a);
+}
+else {
+    console.log(b);
+}
