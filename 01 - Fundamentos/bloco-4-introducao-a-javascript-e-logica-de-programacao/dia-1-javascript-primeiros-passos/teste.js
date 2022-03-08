d442f05e2f359c53a4871b5d8d1872b49c0bb832
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-08 10:40:28 -0300

    Atividade 8 concluída

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -102,7 +102,7 @@ switch(piece.toLowerCase()) {
 }*/
 
 //Exercício 7
-let nota = 92;
+/*let nota = 92;
 
 if( (nota > 100) || (nota < 0)) {
   console.log("Erro");
@@ -124,4 +124,16 @@ else if(nota >= 50) {
 }
 else if(nota < 50) {
   console.log("F");
+}*/
+
+//Exercício 8
+let a = 5;
+let b = 2;
+let c = 3;
+
+if(a %2 == 0 || b %2 == 0 || c %2 == 0) {
+  console.log("true");
+}
+else {
+  console.log("false");
 }
