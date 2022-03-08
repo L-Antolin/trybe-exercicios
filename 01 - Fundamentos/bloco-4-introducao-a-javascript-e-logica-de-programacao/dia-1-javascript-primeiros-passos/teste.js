ca3390886d34a65ce46d2314a76307909b4e4121
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-08 10:45:23 -0300

    Exercício 9 feito, similar ao 8

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -127,11 +127,23 @@ else if(nota < 50) {
 }*/
 
 //Exercício 8
-let a = 5;
-let b = 2;
+/*let a = 5;
+let b = 7;
 let c = 3;
 
 if(a %2 == 0 || b %2 == 0 || c %2 == 0) {
+  console.log("true");
+}
+else {
+  console.log("false");
+}*/
+
+//Exercício 9
+let a = 9;
+let b = 3;
+let c = 5;
+
+if(a %2 != 0 || b %2 != 0 || c %2 != 0) {
   console.log("true");
 }
 else {
