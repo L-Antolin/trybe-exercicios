866e6202048215610f5d17fce0788d7caeae33d1
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-08 12:42:46 -0300

    Atividades exercicios/4.1 finalizados

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -151,7 +151,7 @@ else {
 }*/
 
 //Exercício 10
-let valorCusto = 8;
+/*let valorCusto = 8;
 let valorVenda = 20;
 let valorCustoTotal = valorCusto + valorCusto*0.2;
 let lucro = valorVenda - valorCustoTotal;
@@ -161,4 +161,41 @@ if(valorCusto <=0 || valorVenda <=0) {
 }
 else {
   console.log(1000*lucro);
+}*/
+
+//Exercício 11
+let salarioBruto = 2800;
+let salarioLiquido;
+let salarioBase;
+
+if(salarioBruto < 1556.94) {
+  salarioBase = salarioBruto - (salarioBruto*0.08);
 }
+else if(salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  salarioBase = salarioBruto - (salarioBruto*0.09);
+}
+else if(salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  salarioBase = salarioBruto - (salarioBruto*0.11);
+}
+else if(salarioBruto > 5189.82) {
+  salarioBase = salarioBruto - 570.88;
+}
+
+if(salarioBase <= 1903.98) {
+  salarioLiquido = salarioBase;
+}
+else if(salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  salarioLiquido = salarioBase - ((salarioBase*0.075)-142.80);
+}
+else if(salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+  salarioLiquido = salarioBase - ((salarioBase*0.15)-354.80);
+}
+else if(salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  salarioLiquido = salarioBase - ((salarioBase*0.225)-636.13);
+}
+else if(salarioBase > 4664.68) {
+  salarioLiquido = salarioBase - ((salarioBase*0.275)-869.36);
+}
+/** Para alterar a quatidade de casas decimais pesquisei e encontrei o toFixed no site tutorials point
+Source: https://www.tutorialspoint.com/How-to-format-a-number-with-two-decimals-in-JavaScript */
+console.log(salarioLiquido.toFixed(2));
