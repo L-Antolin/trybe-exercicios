e73942da60a643a2fb822748f04f9fa32f15906f
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-08 00:24:00 -0300

    Exercício 6 finalizado e funcionando

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/teste.js
@@ -52,7 +52,7 @@ else {
 }*/
 
 //Exercício 5
-const anguloA = 30;
+/*const anguloA = 30;
 const anguloB = 60;
 const anguloC = 90;
 
@@ -66,4 +66,39 @@ else if(anguloA+anguloB+anguloC == 180) {
 
 else {
   console.log("false")
-} 
+}*/
+
+//Exercício 6 - movimento de peças encontrado no link : https://www.chesshouse.com/pages/chess-rules 
+//e toLowerCase encontrado no link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+let piece = "Queen";
+
+switch(piece.toLowerCase()) {
+  case "king" : 
+    console.log("Moves from its square to a neighboring square");
+    break;
+
+  case "rook" :
+    console.log("Move in its line or row");
+    break;
+
+  case "bishop" :
+    console.log("Moves diagonally");
+    break;
+  
+  case "queen" :
+    console.log("Move like a Rook or a Bishop");
+    break;
+
+  case "knight" :
+    console.log("Jumps in making the shortest move that is not a straight one");
+    break;
+
+  case "pawn" :
+    console.log("Moves one square straight ahead");
+    break;
+
+  default:
+    console.log("Error, invalid piece!");
+
+}
+
