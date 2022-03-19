2312a2d7a31f8d589c5e7bd020fd587b7e3f7475
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-19 17:19:18 -0300

    Atividade 2 completa

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/teste.js
@@ -4,3 +4,11 @@ for (let i = 2; i <= 10; i +=1) {
   fatorial = fatorial * i;
 }
 console.log(fatorial);
+
+// Atividade 2
+let word = 'banana';
+let palavraIvertida = '';
+for (let i = word.length -1; i >= 0; i -=1) {
+  palavraIvertida += word[i];
+}
+console.log(palavraIvertida);
