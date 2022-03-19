10f035e92e4f4e3feb0c53f308e4145760ecc196
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-19 18:09:27 -0300

    Atividade 3 completa

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/teste.js
@@ -12,3 +12,21 @@ for (let i = word.length -1; i >= 0; i -=1) {
   palavraIvertida += word[i];
 }
 console.log(palavraIvertida);
+
+// Atividade 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = '';
+for (let i = 0; i < array.length; i +=1) {
+  if (maiorPalavra.length < array[i].length) {
+    maiorPalavra = array[i];
+  }
+}
+console.log(maiorPalavra);
+
+let menorPalavra = array[0];
+for (let index = 0; index < array.length; index +=1) {
+  if (menorPalavra.length > array[index].length) {
+    menorPalavra = array[index];
+  }
+}
+console.log(menorPalavra);
