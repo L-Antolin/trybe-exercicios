8147c5d5d722769a26be1db20ea11300dbfd13fe
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-20 16:09:52 -0300

    Atividade 3 e 4  adicionas

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
@@ -9,6 +9,15 @@ let info = {
 console.log('Bem vinda, ' + info.personagem);
 
 // Atividade 2
-info.recorrente = 'Sim',
+info.recorrente = 'Sim';
 console.log(info);
 
+// Atividade 3
+for (chaves in info) {
+  console.log(chaves);
+}
+
+// Atividade 4
+for (valores in info) {
+  console.log(info[valores]);
+};
