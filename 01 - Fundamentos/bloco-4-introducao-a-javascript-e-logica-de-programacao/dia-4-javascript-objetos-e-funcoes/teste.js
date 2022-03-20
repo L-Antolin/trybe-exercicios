d125f89c93099d2ac40bd5d936c4197201aebd57
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-20 16:43:14 -0300

    Atividade 5  adiciona

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
@@ -6,18 +6,39 @@ let info = {
 };
 
 // Atividade 1
-console.log('Bem vinda, ' + info.personagem);
+//console.log('Bem vinda, ' + info.personagem);
 
 // Atividade 2
 info.recorrente = 'Sim';
-console.log(info);
+//console.log(info);
 
 // Atividade 3
 for (chaves in info) {
-  console.log(chaves);
+ // console.log(chaves);
 }
 
 // Atividade 4
 for (valores in info) {
-  console.log(info[valores]);
+  //console.log(info[valores]);
 };
+
+// Atividade 5
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+for (concat in info, info2) {
+  if (info[concat] !== info2[concat]) {
+    console.log(info[concat] + ' e ' + info2[concat]);
+  } else {
+    console.log('Ambos são recorrentes');
+  }
+};
+
+// Atividade 6
+
+// Atividade 7
+
+// Atividade 8
