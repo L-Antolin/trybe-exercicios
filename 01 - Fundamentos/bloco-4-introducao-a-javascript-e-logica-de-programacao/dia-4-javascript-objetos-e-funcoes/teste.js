c4918a29b8b8ab06fe4ab901b2d581ad51694ec2
Lanna Antolin <lanna.antolin@gmail.com>
2022-03-20 15:56:55 -0300

    Atividade 2 adicionada e atualizações feitas

--- 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
+++ 01 - Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/teste.js
@@ -1,7 +1,14 @@
+// Parte I - Objetos e For/In
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+// Atividade 1
 console.log('Bem vinda, ' + info.personagem);
+
+// Atividade 2
+info.recorrente = 'Sim',
+console.log(info);
+
